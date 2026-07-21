10b90a6ac35fc6f685262f4918e1a2baa2227b5d
Joice Omer <Joicelogy@gmail.com>
2026-07-21 11:58:47 +0200

    is the hero going to surive the dragons

---
+++ Ishegonnasurvive.js
@@ -0,0 +1,10 @@
+// codewarLink https://www.codewars.com/kata/59ca8246d751df55cc00014c
+function hero(bullets, dragons){
+if (bullets >= dragons*2 ){
+  return true
+}
+  else{
+    return false
+  }
+}
+console.log(hero(4,2))
